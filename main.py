c11967da708ac99aef5c7ca780677ce99b42d9e4
Dominik Wolny <dominik.wolny@doitsoftware.pl>
2020-11-13 18:51:55 +0100

    added path example, updated gitignore

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
+from pathlib import Path
 
+
+train_path = Path('training_images')
+live_test_path = Path('live_test_images')
+
+# file_to_open = path_variable/'filename_string'
+# f = open(file_to_open)
+# print(f.read())
 
 print(tf.__version__)
