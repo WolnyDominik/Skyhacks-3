8ff4a3c3d26363fc79631a02e21d3d538121c2e5
Havystar <31964929+Havystar@users.noreply.github.com>
2020-11-13 21:41:53 +0100

    fix generator

--- answerGenerator.py
+++ answerGenerator.py
@@ -13,9 +13,11 @@ columns = ['Name', 'Amusement park', 'Animals', 'Bench', 'Building', 'Castle',
        'Open-air museum', 'Park', 'Person', 'Plants', 'Reservoir', 'River',
        'Road', 'Rocks', 'Snow', 'Sport', 'Sports facility', 'Stairs', 'Trees',
        'Watercraft', 'Windows']
-defaultOutputRow = np.full((500), False, dtype=bool)
-df = pd.DataFrame() 
-for column in columns:
-    df[column] = defaultOutputRow
+defaultOutputRow = np.full((39), False, dtype=bool)
+df = pd.DataFrame(data=None, index=None, columns=columns, dtype=None, copy=False) 
+
+for i in range(500):
+    df.loc[i] = defaultOutputRow
 df['Name']=files
 df.to_csv('output.csv', index=False)
+print(defaultOutputRow.shape)
