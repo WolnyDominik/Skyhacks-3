48bdcbfa6a78134fb1eff9b5082760d1d91b8217
Havystar <31964929+Havystar@users.noreply.github.com>
2020-11-14 00:19:53 +0100

    fix generator

--- answerGenerator.py
+++ answerGenerator.py
@@ -5,7 +5,6 @@ import os
 path = './live_test_images/'
 
 files = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
-training_labels = pd.read_csv('training_labels.csv')
 columns = ['Name', 'Amusement park', 'Animals', 'Bench', 'Building', 'Castle',
        'Cave', 'Church', 'City', 'Cross', 'Cultural institution', 'Food',
        'Footpath', 'Forest', 'Furniture', 'Grass', 'Graveyard', 'Lake',
@@ -13,11 +12,13 @@ columns = ['Name', 'Amusement park', 'Animals', 'Bench', 'Building', 'Castle',
        'Open-air museum', 'Park', 'Person', 'Plants', 'Reservoir', 'River',
        'Road', 'Rocks', 'Snow', 'Sport', 'Sports facility', 'Stairs', 'Trees',
        'Watercraft', 'Windows']
-defaultOutputRow = np.full((39), False, dtype=bool)
+defaultOutputRow = np.full((39), 1, dtype=int)
 df = pd.DataFrame(data=None, index=None, columns=columns, dtype=None, copy=False) 
 
 for i in range(500):
     df.loc[i] = defaultOutputRow
 df['Name']=files
-df.to_csv('output.csv', index=False)
-print(defaultOutputRow.shape)
+df.to_csv('outputFile.csv', index=False)
+
+
+
