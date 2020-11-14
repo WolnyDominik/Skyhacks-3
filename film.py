ef374280278e2691acecc57c19b098b4437fc7ed
Dominik Wolny <dominik.wolny@doitsoftware.pl>
2020-11-14 22:50:18 +0100

    Added film algo file

---
+++ film.py
@@ -0,0 +1,25 @@
+import sys
+from model_manager import *
+import os
+
+step = 1
+film_name = sys.argv[1]
+czesto_path = os.path.join(film_data_folder, 'czesto')
+
+manager = Manager()
+manager.loadCsv()
+
+film_to_frames(film_name, step=step)
+
+processAllImagesToBinary(images_path=czesto_path, bin_path=film_bin_path) # to wywołać tylko i wyłącznie raz
+manager.importTrainingDataFromBinaryFile(path_to_file=film_bin_path)
+
+manager.loadModel('to-dalej-przegrywamy.h5')
+
+manager.predictAndSaveToCsv(
+    images_path=czesto_path,
+    binary_path=film_bin_path, 
+    path_to_csv=os.path.join(film_data_folder, "film.csv"), 
+    treshold = 0.2)
+
+process_film_csv(jump=step, treshold=1)
