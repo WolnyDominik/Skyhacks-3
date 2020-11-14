b0f9f3ae1bdf78e02b0cb6c8c635604db1b0eb1a
Mikkhael <35925706+Mikkhael@users.noreply.github.com>
2020-11-14 16:08:53 +0100

    ładniejszy node

--- node/public/index.js
+++ node/public/index.js
@@ -1,39 +1,65 @@
 
 let processingIds = {};
 
-function sendVideo(input)
+
+function setup()
 {
-    console.log("Sending Video");
+    document.getElementById("file_upload_video").addEventListener('change', (e)=>{
+        uploadFileForAnalisys(e.target, "video");
+    });
+    document.getElementById("file_upload_audio").addEventListener('change', (e)=>{
+        uploadFileForAnalisys(e.target, "audio");
+    });
+    
+    uploadButtons = document.getElementsByClassName("upload_button");
+    for( element of uploadButtons) {
+        element.addEventListener('click', e => {
+            document.getElementById(e.target.getAttribute('destination_file_input')).click();
+        });
+    }
+}
+
+setup();
+
+function createComponent(id, classname = "", innerHTML = "")
+{
+    let component = document.createElement('section');
+    component.setAttribute("class", "component " + classname);
+    component.setAttribute("id", id);
+    component.innerHTML = innerHTML;
+    document.getElementById("components_container").appendChild(component);
+}
+
+function createProcessingComponent(id, type, file)
+{
+    const template=`
+        <header>Processing ${type} file: <span class="filename">${file.name}</span></header>
+        <section class="status"> 
+            <section> Progress: <span id="${id}_progress_counter" >0<span>% </section>
+        </section>
+        <section class="result">
+            <img src="" style="display:none">
+        </section>
+    `;
+    createComponent(id, "processing", template);
+}
+
+
+function uploadFileForAnalisys(input, type)
+{
+    console.log("Sending " + type);
         
     var data = new FormData();
     data.append('file', input.files[0]);
     
-    fetch("/upload/video",
+    fetch("/upload/" + type,
     {
         method: "POST",
         body: data
     })
     .then(function(res){ return res.json(); })
-    .then(function(data){ processingIds[data.id] = true; })
+    .then(function(data){ createProcessingComponent(data.id, type, input.files[0]) })
     .catch(function(err){ console.log(err); })
     
-    console.log("Video Send");
-}
-function sendAudio(input)
-{
-    console.log("Sending Audio");
-    
-    var data = new FormData();
-    data.append('file', input.files[0]);
-    
-    fetch("/upload/audio",
-    {
-        method: "POST",
-        body: data
-    })
-    .then(function(res){ return res.json(); })
-    .then(function(data){ processingIds[data.id] = true; })
-    .catch(function(err){ console.log(err); })
-    
-    console.log("Audio Send");
+    console.log(type + " send");
 }
